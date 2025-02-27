fa768e7f2a7ec1d8f2abb14c9cefe98d2b9153b8
Shadowbeasto7 <126560486+Shadowbeasto7@users.noreply.github.com>
2025-02-27 12:29:24 +0545

    Create add_books.php

---
+++ add_books.php
@@ -0,0 +1,37 @@
+<?php
+include "database.php";
+include "tables.php";
+include "header.php";
+$obj=new database();
+?>
+<form id="add-form">
+
+<label for="subject">Choose subject related to book:</label>
+<select  id="subject">
+<option value="">Select subject</option>
+</select>
+<br>
+<br>
+<label for="author">Select the author of book:</label>
+<select  id="author-1">
+<option value="">Select author</option>
+</select>
+<br>
+<br>
+<label for="publication">Choose the textbook :</label>
+<select  id="textbooks-1">
+<option value="">Select Textbooks</option>
+</select>
+<br>
+<br>
+
+<label for="Quantity">Enter the Quantity of this book:</label>
+<input type="number" id="quantity">
+
+<input type="submit" id="save-books" value="save">
+</form>
+<div id="success-msg"></div>
+<div id="error-msg"></div>
+<?php
+   include('footer.php');
+?>
