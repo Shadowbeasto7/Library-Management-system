468b6b3b47216099b069fe14fdd60aa6cb08d084
Shadowbeasto7 <126560486+Shadowbeasto7@users.noreply.github.com>
2025-03-07 15:13:05 +0545

    Create add_textbook.php

---
+++ add_textbook.php
@@ -0,0 +1,24 @@
+<?php
+include "database.php";
+include "tables.php";
+include "header.php";
+include "sidebar.php";
+$obj = new database();
+?>
+<div class="container-fluid">
+   <div class="row">
+      <div class="col-xl-12 col-lg-12 col-md-12 col-sm-12 col-12">
+         <div class="page-header">
+            <h2 class="pageheader-title">Add Textbook</h2>
+            <div class="page-breadcrumb">
+               <nav aria-label="breadcrumb">
+                  <ol class="breadcrumb">
+                     <li class="breadcrumb-item"><a href="#" class="breadcrumb-link">Dashboard</a></li>
+                     <li class="breadcrumb-item"><a href="#" class="breadcrumb-link">Textbook</a></li>
+                     <li class="breadcrumb-item active" aria-current="page">Add Textbook</li>
+                  </ol>
+               </nav>
+            </div>
+         </div>
+      </div>
+   </div>
